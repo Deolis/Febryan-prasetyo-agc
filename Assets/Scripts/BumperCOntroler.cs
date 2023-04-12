6025fe4a4ad5bc4b492b4d54d8846d2ff5530db6
febryanp <this.febryan@gmail.com>
2023-04-13 00:42:56 +0700

    Ball paddle bumper

--- Assets/Scripts/BumperCOntroler.cs
+++ Assets/Scripts/BumperCOntroler.cs
@@ -12,18 +12,23 @@ public class BumperCOntroler : MonoBehaviour
     [SerializeField]
     private Color color;
     private Renderer render;
+    private Animator animator;
 
     private void Start()
     {
         render = GetComponent<Renderer>();
         render.material.color = color;
+        animator = GetComponent<Animator>();
+        
     }
     private void OnCollisionEnter(Collision collision)
     {
+        
         if (collision.collider == bola)
         {
             Rigidbody bolaRig = bola.GetComponent<Rigidbody>();
             bolaRig.velocity *= multiplayer;
+            animator.SetTrigger("Hits");
         }
 
     }
